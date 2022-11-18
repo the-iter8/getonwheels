dbff3481fbb346b724d786001df2f38f2eb0e121
iter8 <randomdweller.me@gmail.com>
2022-11-19 02:41:35 +0530

    About us page

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,7 +1,85 @@
-import React from 'react'
+import React, { useContext } from 'react';
+// import { UserContext } from '../pages/_app';
+import styles from './Footer.module.css';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import Logo from '../../public/Logo.png';
+
+import Text from '../Text/Text';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import PinterestIcon from '@mui/icons-material/Pinterest';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+
+import { Container, Row, Col } from 'react-grid-system';
 
 export default function Footer() {
+  const dataq = {
+    product: [
+      'Overview',
+      'Features',
+      'Solutions',
+      'Tutorials',
+      'Pricing',
+      'Releases',
+    ],
+    company: ['About us', 'Careers', 'Press', 'News', 'Media kit', 'Contact'],
+    resource: [
+      'Blog',
+      'Newsletter',
+      'Events',
+      'Help centre',
+      'Tutorials',
+      'Support',
+    ],
+    social: [
+      'Twitter',
+      'LinkedIn',
+      'Facebook',
+      'GitHub',
+      'AngelList',
+      'Dribbble',
+    ],
+  };
+
+  function Column({ colName, items }) {
+    console.log(items);
+    return (
+      <Col className={styles.column} xs={6} sm={6} lg={4}>
+        <Text size='md'>{colName}</Text>
+        {items.map((item) => {
+          return (
+            <Text size='sm' color='grey-white' variant='a' href='/services'>
+              {item}
+            </Text>
+          );
+        })}
+      </Col>
+    );
+  }
   return (
-    <div>Footer</div>
-  )
+    <div className={styles.footerUpper}>
+      <div className={styles.footerUpperText}>
+        <Image alt='logo' src={Logo} height={60} />
+        <Text size='md'>
+          Design amazing digital experiences that create more happy in the
+          world.
+        </Text>
+        <div className={styles.footerUpperLogos}>
+          <PinterestIcon fontSize='large'></PinterestIcon>
+          <YouTubeIcon fontSize='large'></YouTubeIcon>
+          <FacebookIcon fontSize='large'></FacebookIcon>
+        </div>
+      </div>
+      <div>
+        <Container>
+          <Row gutterWidth={120}>
+            <Column colName={'Product'} items={dataq.product}></Column>
+            <Column colName={'Company'} items={dataq.company}></Column>
+            <Column colName={'Resource'} items={dataq.resource}></Column>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 }
--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,134 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import styles from '../styles/aboutUs.module.css';
+import Text from '../components/Text/Text';
+import { UserContext } from '../pages/_app';
+import { Container, Row, Col } from 'react-grid-system';
 
 export default function AboutUs() {
+  const { isMobile } = useContext(UserContext);
+  function About() {
+    function Item() {
+      return (
+        <div className={styles.aboutItem}>
+          <Text size='huge' weight='bold' color='dark-blue'>
+            400+
+          </Text>
+          <Text size='sm' weight='bold'>
+            Projects Completed.
+          </Text>
+          <Text size='xs' color='grey-white'>
+            We've helped build over 400 amazing projects.
+          </Text>
+        </div>
+      );
+    }
+
+    return (
+      <div className={styles.aboutRoot}>
+        <div className={styles.aboutTitles}>
+          <div>
+            <Text size='sm' color='crusta'>
+              {' '}
+              About Us
+            </Text>
+            <Text size='xxxl' weight='bold'>
+              We do things differently...
+            </Text>
+          </div>
+          <div>
+            <Text size='md' weight='semi-bold' variant='a' href='/'>
+              Learn More about the company and the team behind it.
+            </Text>
+          </div>
+        </div>
+        <div>
+          <Text align='center'>-</Text>
+        </div>
+        <Container>
+          <Row gutterWidth={50}>
+            <Col sm={12} md={6} lg={3} className={styles.column}>
+              <Item></Item>
+            </Col>
+            <Col sm={12} md={6} lg={3} className={styles.column}>
+              <Item></Item>
+            </Col>
+            <Col sm={12} md={6} lg={3} className={styles.column}>
+              <Item></Item>
+            </Col>
+            <Col sm={12} md={6} lg={3} className={styles.column}>
+              <Item></Item>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+
+  function Story() {
+    return (
+      <div className={styles.storyRoot}>
+        <div className={styles.storyTitles}>
+          <Text size='sm' color='crusta'>
+            Out Story
+          </Text>
+          <Text size='xxxl' weight='bold'>
+            We are just getting started.
+          </Text>
+          <Text size='sm' color='grey-white' weight='semi-bold'>
+            We've already helped over 4,000 companies achieve remarkable
+            results.
+          </Text>
+        </div>
+
+        <Container>
+          <Row gutterWidth={50}>
+            <Col md={12} lg={6} className={styles.column}>
+              <Text size='sm' color='grey-white'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio a
+                porro enim consequuntur fuga libero, dignissimos incidunt
+                reprehenderit neque exercitationem officiis similique hic
+                aspernatur minima quae iusto possimus voluptatum fugiat amet?
+                Deleniti, ducimus culpa repellendus, id ipsa debitis recusandae
+                quia eaque dolorem possimus tempore veniam nisi? Voluptatem
+                dicta eum iusto.
+              </Text>
+              <Text size='sm' color='grey-white'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio a
+                porro enim consequuntur fuga libero, dignissimos incidunt
+                reprehenderit neque exercitationem officiis similique hic
+                aspernatur minima quae iusto possimus voluptatum fugiat amet?
+                Deleniti, ducimus culpa repellendus, id ipsa debitis recusandae
+                quia eaque dolorem possimus tempore veniam nisi? Voluptatem
+              </Text>
+            </Col>
+            <Col md={12} lg={6} className={styles.column}>
+              <Text size='sm' color='grey-white'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio a
+                porro enim consequuntur fuga libero, dignissimos incidunt
+                reprehenderit neque exercitationem officiis similique hic
+                aspernatur minima quae iusto possimus voluptatum fugiat amet?
+                Deleniti, ducimus culpa repellendus, id ipsa debitis recusandae
+                quia eaque dolorem possimus tempore veniam nisi? Voluptatem
+                dicta eum iusto.
+              </Text>
+              <Text size='sm' color='grey-white'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio a
+                porro enim consequuntur fuga libero, dignissimos incidunt
+                reprehenderit neque exercitationem officiis similique hic
+                aspernatur minima quae iusto possimus voluptatum fugiat amet?
+                Deleniti, ducimus culpa repellendus, id ipsa debitis recusandae
+                quia eaque dolorem possimus tempore veniam nisi? Voluptatem
+              </Text>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
   return (
-    <div>AboutUs</div>
-  )
+    <>
+      <About></About>
+      <Story></Story>
+    </>
+  );
 }
